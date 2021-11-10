679eb717b25aff233200e9b9504a4a96c3624c2e
Swan Chase <swancd@gmail.com>
2021-11-10 15:29:23 +0100

    added start up files for shooting throwing and the boss

--- Assets/Scripts/ClunkShoot.cs
+++ Assets/Scripts/ClunkShoot.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class ClunkShoot : MonoBehaviour
 {
     [SerializeField]
+    GameObject barrel;
+    [SerializeField]
+    GameObject bullet;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+
+        Instantiate(bullet, barrel.transform);
     }
 }
