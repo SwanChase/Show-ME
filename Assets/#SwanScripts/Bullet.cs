1a6fd860e74c6a9697eebd6184096f702ed75b17
Swan Chase <swancd@gmail.com>
2021-11-11 13:58:46 +0100

    dif name script folder

---
+++ Assets/#SwanScripts/Bullet.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject particalSystem;
+
+    [SerializeField]
+    float movespeed = 4;
+    float deathTimer = 5;
+
+    [SerializeField]
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, deathTimer);
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * movespeed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.GetContact(0);
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            if (particalSystem!)
+            {
+                Instantiate(particalSystem, contact.point, Quaternion.LookRotation(contact.normal));
+                particalSystem.transform.parent = null;
+                Disable();
+            }
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+        
+    }
+    private void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
